7ebc1356d4c8e6b3966c9d94976abff728d19f91
Saboor Ahmad <saboorahm7e@gmail.com>
2024-08-30 18:56:00 +0500

    Initial Commit

---
+++ ExploreItaly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExploreItaly
+//
+//  Created by Saboor on 30/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExploreItaly/ExploreItalyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreItalyApp.swift
+//  ExploreItaly
+//
+//  Created by Saboor on 30/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExploreItalyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
